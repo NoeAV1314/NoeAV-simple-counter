31b150157f13b4f723ef8ad3b88a29af552c2e08
Noe CM <128952542+NoeAV1314@users.noreply.github.com>
2024-01-25 22:13:35 +0000

    Simple counter logic done. Calendar issue

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,45 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
-
-// include your styles into the webpack bundle
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
 import "../styles/index.css";
+import 'bootstrap/dist/css/bootstrap.css';
 
-//import your own components
-import Home from "./component/home.jsx";
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { icon } from '@fortawesome/fontawesome-svg-core/import.macro'
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+function SimpleCounter(props){
+    return (<div className="bigCounter">
+                <div className="calendar">0</div>
+                <div className="squareFour">0 {props.digitFour}</div>
+                <div className="squareThree">0 {props.digitThree}</div>
+                <div className="squareTwo">0 {props.digitTwo}</div>
+                <div className="squareOne">0 {props.digitOne}</div>
+            </div>);
+}
+MyComponent.propTypes = {
+    // You can declare that a prop is a specific JS primitive. By default, these
+    // are all optional.
+    digitFour: PropTypes.number,
+    digitThree: PropTypes.number,
+    digitTwo: PropTypes.number,
+    digitOne: PropTypes.number,
+
+}    
+
+let counter = 0;
+
+setInterval(function(){
+    const squareFour = Math.floor(counter/10000);
+    const squareThree = Math.floor(counter/1000);
+    const squareTwo = Math.floor(counter/100);
+    const squareOne = Math.floor(counter/10);
+    console.log (squareOne, squareTwo, squareThree, squareFour);
+// include your styles into the webpack bundle
+    calendar++;
+    ReactDOM.render(
+        <SimpleCounter digitOne={squareOne} digitTwo={squareTwo} digitThree={squareThree} digitFour={squareFour} />,
+        document.querySelector('#app')
+    );
+}, 1000);
